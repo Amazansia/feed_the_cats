7c7f04b44e85c407f7371a8c9d1452efb2edf4f9
Amazansia <asd111050@naver.com>
2022-05-17 18:42:09 +0900

    JAVA 재활 - 반복문

---
+++ src/반복문/boj10951.java
@@ -0,0 +1,17 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class boj10951 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		while (sc.hasNextInt()) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A + B);
+		}
+
+	}
+}
---
+++ src/반복문/boj2741.java
@@ -0,0 +1,14 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class boj2741 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 1;i<=N;i++){
+			System.out.println(i);
+		}
+	}
+
+}
